83cfd6de75f3bb16cddf2ced628adfcaefdfaa07
Roman_Golovin1 <romagolovin@gmail.com>
2021-12-01 14:07:11 +0500

    added before and after methods

---
+++ src/test/java/com/ppg/firstTest.java
@@ -0,0 +1,38 @@
+package com.ppg;
+
+import org.junit.jupiter.api.*;
+
+public class firstTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("This is the before all method!");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("This is the after all method!");
+    }
+
+    @BeforeEach
+    void openYandex() {
+        System.out.println("        open(\"ya.ru\")");
+    }
+
+    @AfterEach
+    void takeScreen() {
+        System.out.println("        takeScreen();");
+    }
+
+    @Test
+    void firstTest1() {
+        System.out.println("                     first test()");
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("                    second test()");
+        Assertions.assertTrue(true);
+    }
+}
